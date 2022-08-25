76bc5857cd22bb060a3a802ff570169f65b837e1
dev global <demoglobal@gmail.com>
2022-08-25 16:07:15 +0530

    update EmployeeController and Employee domain class

--- demo-1/src/main/java/com/example/demo/model/Employee.java
+++ demo-1/src/main/java/com/example/demo/model/Employee.java
@@ -31,9 +31,6 @@ public class Employee {
 	@Column(name="email")
 	private String email;
 	
-	@Column(name="salary")
-	private int salary;
-	
 	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name="ea_id")
 	private EmployeeAddress employeeAddress;
